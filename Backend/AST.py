0ccdc60836e68eb2b2f8a36c661e4a4d5766e1ca
ieepirzy <ieepirzy@jyu.fi>
2025-11-12 13:26:01 +0200

    added llvm compatability to vardecnode

--- Backend/AST.py
+++ Backend/AST.py
@@ -5,11 +5,13 @@ class ProgramNode(AstNode): #The "root" of the AST.
     def __init__(self):
         self.statements = []
 
-class varDeclNode(AstNode): #LLVM IR can allocate memory and store the initial value.
-    def __init__(self,name,value, typ=None):
-        self.name = name
-        self.value = value
-        self.type = typ
+class VarDeclNode(AstNode):
+    def __init__(self, name, value, typ=None, kind="var"):
+        self.name = name            # variable name
+        self.value = value          # expression node
+        self.type = typ             # LLVM type string
+        self.kind = kind            # 'var' or 'const'
+    
 
 class binaryExprNode(AstNode): #Contains structure to represent a binary expression: 
     def __init__(self,left,op,right):
